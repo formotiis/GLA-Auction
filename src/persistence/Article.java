82a87fcfa4850f6dccb9a882ed069a8c90b89768
Charlotte <charlotte@MacBook-Pro-de-Charlotte.local>
2020-02-13 15:58:34 +0100

    commit a ignorer

---
+++ src/persistence/Article.java
@@ -0,0 +1,17 @@
+package persistence;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Article implements Serializable {
+
+    private int ownerId;
+    private String name;
+    private float minPrice;
+    private String categorie;
+    private Date date;
+
+    public Article
+
+
+}
