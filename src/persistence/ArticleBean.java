8d580897fc7d4b814d2dd89bf4bd6de0503d84b9
ChaLan13 <charlotte.lanuel@hotmail.fr>
2020-02-25 15:50:15 +0100

    form article

--- src/persistence/ArticleBean.java
+++ src/persistence/ArticleBean.java
@@ -5,6 +5,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 @ManagedBean(name="articleBean")
 @SessionScoped
@@ -13,11 +15,30 @@ public class ArticleBean implements Serializable {
     @Inject
     ArticleManager articleManager;
 
+    private Person owner;
+    private String name;
+    private Long id;
+    private Double price;
+    private Date end;
+    List<String> categories;
+    private String description;
+
     public ArticleBean(){
 
     }
 
+    public List<Article> getAvailable(){
+        return articleManager.getAllAvailableArticles();
+    }
+
     public String goBack(){
         return "index";
+    }
+
+
+    public String addArticle(){
+        Article a = new Article(owner, name, price, end, categories.toString(), description);
+        articleManager.addNew(a);
+        return "article ajouté";
     }
 }
