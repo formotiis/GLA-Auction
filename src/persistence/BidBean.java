75e4d0096b0c8ebd682fd2eb3f5cd88c88eb406c
Highnewb <thouveninvalentin@gmail.com>
2020-02-26 10:37:43 +0100

    Ajouts sur BidBean

--- src/persistence/BidBean.java
+++ src/persistence/BidBean.java
@@ -12,9 +12,44 @@ public class BidBean implements Serializable {
     @Inject
     BidManager bidManager;
 
+    private int id;
+    private int ownerId;
+    private int itemId;
+    private float bidValue;
 
-    //TODO: Delet dis
-    public void donothing(Double b){
-        System.nanoTime();
+    public BidBean(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public float getBidValue() {
+        return bidValue;
+    }
+
+    public void setBidValue(float bidValue) {
+        this.bidValue = bidValue;
     }
 }
